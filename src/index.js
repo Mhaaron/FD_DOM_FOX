737dfa55057b7970f36659c93803d316c1711cee
mhaaron <s14120066@alumnos.itsur.edu.mx>
2021-09-06 21:47:27 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,55 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { registerImg, reportLoadImg, clearCounter } from "./lazy"
 
-console.log('Happy hacking :)')
+/* https://randomfox.ca/images/2.jpg */
+
+const BASEURLIMG = 'https://randomfox.ca/images/'
+const MOUNTNODE = document.getElementById('images')
+const ADDBUTTON = document.getElementById('btnAdd')
+const REMOVEBUTTON = document.getElementById('btnRm')
+
+let counterAddImg = 0
+
+const createImgNode = () => {
+    const container = document.createElement('div')
+    container.className = 'p-4'
+    
+    const img = document.createElement('img')
+    img.dataset.src = `${BASEURLIMG}${Math.round(Math.random() * (123 - 1) + 1)}.jpg`
+    img.className = 'mx-auto'
+    img.style = 'width: 320px'
+
+    const wrapperImg = document.createElement('div')
+    wrapperImg.className = 'bg-gray-200'
+    wrapperImg.style.minHeight = '150px'
+    wrapperImg.style.display = 'inline-block'
+    wrapperImg.appendChild(img)
+    
+    container.append(wrapperImg)
+    
+    counterAddImg++
+    reportAddImg()
+    reportLoadImg()
+
+    return container
+}
+
+const addImg = () => {
+    const newImg = createImgNode()
+    MOUNTNODE.append(newImg)
+    registerImg(newImg)
+}
+
+export const reportAddImg = () => {
+    console.log('-----------------')
+    console.log(`Imagenes agregadas: ${counterAddImg}`)
+}
+
+const removeImg = () => {
+    MOUNTNODE.innerHTML = ""
+    counterAddImg = 0
+    clearCounter()
+    console.clear()
+}
+
+ADDBUTTON.addEventListener('click', addImg)
+REMOVEBUTTON.addEventListener('click', removeImg)
